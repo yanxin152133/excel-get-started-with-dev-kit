2d7e4d7567e058547845127c92a8c3ace2b98295
yanxin152133 <yanxin152133@gmail.com>
2025-09-25 02:18:33 +0800

    添加多个按钮以支持不同颜色的填充功能，并更新相应的事件处理程序

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,6 +10,9 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+    document.getElementById("run1").onclick = run1;
+    document.getElementById("run2").onclick = run2;
+    document.getElementById("run3").onclick = run3;
   }
 });
 
@@ -23,6 +26,63 @@ export async function run() {
 
       // Update the fill color.
       range.format.fill.color = "black";
+
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function run1() {
+  try {
+    await Excel.run(async (context) => {
+      const range = context.workbook.getSelectedRange();
+
+      // Read the range address.
+      range.load("address");
+
+      // Update the fill color.
+      range.format.fill.color = "yellow";
+
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function run2() {
+  try {
+    await Excel.run(async (context) => {
+      const range = context.workbook.getSelectedRange();
+
+      // Read the range address.
+      range.load("address");
+
+      // Update the fill color.
+      range.format.fill.color = "red";
+
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function run3() {
+  try {
+    await Excel.run(async (context) => {
+      const range = context.workbook.getSelectedRange();
+
+      // Read the range address.
+      range.load("address");
+
+      // Update the fill color.
+      range.format.fill.color = "green";
 
       await context.sync();
       console.log(`The range address was ${range.address}.`);
